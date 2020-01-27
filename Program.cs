62c5cf3d3caaf3ad00b51c36646354dc4da04a67
Sashfish <dormiii4@gmail.com>
2020-01-27 17:32:13 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Новая_папка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+      bool won = false;
+      int number  = random.Next(1, 10);
+      Console.WriteLine("Guess the number");
+      while (won == false)
+      {
+      string input = Console.ReadLine();
+      int guess = Int32.Parse(input);
+      if (guess == number)
+      {
+        Console.WriteLine("You got it");
+        won = true;
+      }
+      else if (guess < number)
+      {Console.WriteLine("Try higher");}
+      else {Console.WriteLine("Try lower");}
+    }
+        }
+    }
+}
